dd35b7a65864d5134b5eb1f7af00a9dc05abdb96
Jim Grimwood <48063481+grimatksu@users.noreply.github.com>
2022-11-02 15:39:26 -0500

    Update streamlit_app.py
    Added smoothie title
    added smoothie list

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ streamlit.text('Today\'s Menu')
 streamlit.text('Bacon & Eggs')
 streamlit.text('Biscuits & Gravy')
 streamlit.text('Omelette')
+stramlit.title('Build Your Own Smoothie')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
