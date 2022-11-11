d66ceb67e9947ad915f2689413e91fe4fb738382
Jim Grimwood <48063481+grimatksu@users.noreply.github.com>
2022-11-11 09:49:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.header('Today\'s Menu')
 streamlit.text('Bacon & Eggs')
 streamlit.text('Biscuits & Gravy')
 streamlit.text('Omelette')
-#streamlit.title(':strawberry: Build Your Own Smoothie')
+streamlit.title(':strawberry: Build Your Own Smoothie')
 
 
 #my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
