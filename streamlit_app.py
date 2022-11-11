69277fb80d587a9b42fa85cf5fe2e345fa1c3cf5
Jim Grimwood <48063481+grimatksu@users.noreply.github.com>
2022-11-11 09:52:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-#import pandas
+import pandas
 #import requests
 #import snowflake.connector
 #from urllib.error import URLError
@@ -12,8 +12,8 @@ streamlit.text('Omelette')
 streamlit.title(':strawberry: Build Your Own Smoothie')
 
 
-#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 #fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Banana','Strawberries'])
