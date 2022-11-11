42a605112e32a7145e602a977ca8ddc507028abb
Jim Grimwood <48063481+grimatksu@users.noreply.github.com>
2022-11-11 10:24:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalize)
+streamlit.dataframe(fruityvice_normalized)
 
 # Display the table on the page.
 #streamlit.dataframe(fruits_to_show)
